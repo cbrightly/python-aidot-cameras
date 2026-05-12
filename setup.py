ed04cb1fe03e6ea93451208f3ec8c570c19c9169
Bryan <hongjinbin@leedarson.com>
2026-05-12 09:17:02 +0800

    Release v0.3.52

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-aidot",
-    version="0.3.51",
+    version="0.3.52",
     author="aidotdev2024",
     url='https://github.com/Aidot-Development-Team/python-aidot',
     description="aidot control wifi lights",
